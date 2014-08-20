a8b24fcd7615507d5ed9994f8c336b991ab4a811
David Darke <david@atomicsmash.co.uk>
2014-08-20 15:24:19 +0100

    Changed loading classes... wonder where I got those from ;)

--- acf-limiter.php
+++ acf-limiter.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields: Limiter
 Plugin URI: http://wordpress.org/extend/plugins/advanced-custom-fields-limiter-field/
 Description: This field provides a textarea that limits the number of characters the a user can add. The limit is cleanly represented by a jQuery Ui progress bar. You can define the number of characters on a per field basis.
-Version: 1.1.0
+Version: 1.1.1
 Author: Atomic Smash - David Darke
 Author URI: atomicsmash.co.uk
 License: GPLv2 or later
@@ -12,19 +12,19 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 		
 		
-function include_field_types_Gravity_Forms( $version ) {
+function include_field_types_limiter( $version ) {
 
   include_once('limiter-v5.php');
 
 }
 
-add_action('acf/include_field_types', 'include_field_types_gravity_forms'); 
+add_action('acf/include_field_types', 'include_field_types_limiter'); 
 
 
-function register_fields_Gravity_Forms() {
+function register_fields_limiter() {
   include_once('limiter-v4.php');
 }
 
-add_action('acf/register_fields', 'register_fields_gravity_forms');
+add_action('acf/register_fields', 'register_fields_limiter');
 		
 ?>
